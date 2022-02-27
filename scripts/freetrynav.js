6422b1f0b15191c9dcadee0ad900fdfb07a9218c
Ayush Batra <ayushbatra128@gmail.com>
2022-02-27 17:37:46 +0530

    navbar changed for free try page

---
+++ scripts/freetrynav.js
@@ -0,0 +1,14 @@
+import footer from "../components/footer.js";
+import { navigation, searchpart, popuppart } from "../components/nav1.js"
+
+let navItems = document.getElementById('navItems');
+navItems.innerHTML = popuppart()
+let nav = document.getElementById('nav');
+nav.innerHTML = navigation();
+let searchBar = document.getElementById('searchBar');
+searchBar.innerHTML = searchpart();
+
+
+let footerdiv = document.getElementById('footer');
+footerdiv.innerHTML = footer();
+console.log(footer);
